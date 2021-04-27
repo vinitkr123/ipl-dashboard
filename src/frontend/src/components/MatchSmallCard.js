1350adbdb4a554e224d3b53c823babc85a4a6c1f
Vinit Kamboj <vinitkr.kamboj@gmail.com>
2021-04-27 11:55:53 -0500

    added react project

---
+++ src/frontend/src/components/MatchSmallCard.js
@@ -0,0 +1,9 @@
+import {React} from 'react';
+
+export const MatchSmallCard = () => {
+    return (
+        <div className="TeamPage">
+           <p>Match Small Card           </p></div>
+    );
+}
+
